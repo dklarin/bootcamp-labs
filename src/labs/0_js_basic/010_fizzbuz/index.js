86b966d3b38f453fdd91bd4c429d28788a2fdc19
dklarin <danijel.klarin@gmail.com>
2019-05-07 00:31:13 +0200

    Prints the numbers from 1 to 100

--- src/labs/0_js_basic/010_fizzbuz/index.js
+++ src/labs/0_js_basic/010_fizzbuz/index.js
@@ -7,7 +7,17 @@ This Lab Exercise has no input
 `;
 
 function solution(input, output) {
-  
+
+    for (var i = 1; i <= 100; i++) {
+        if (i % 3 == 0 && i % 5 == 0)
+            output.write("FizzBuzzss")
+        else if (i % 3 == 0)
+            output.write("Fizz")
+        else if (i % 5 == 0)
+            output.write("Buzz")
+        else
+            output.write(i)
+    }
 }
 
 export default { id: "010_fizzBuzz", task, solution };
