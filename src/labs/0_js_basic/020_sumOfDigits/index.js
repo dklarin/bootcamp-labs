c8d04a093531b0f1645b65c553b8d8ff363321cf
unknown <danijel.klarin@gmail.com>
2019-05-07 18:41:33 +0200

    summing of digits

--- src/labs/0_js_basic/020_sumOfDigits/index.js
+++ src/labs/0_js_basic/020_sumOfDigits/index.js
@@ -1,3 +1,7 @@
+import {
+    type
+} from "os";
+
 const task = `
 Write a program that will output a sum of digits of a input number
 `;
@@ -5,6 +9,21 @@ Write a program that will output a sum of digits of a input number
 /**the test case inputs */
 var testCases = [12345, 123345];
 
-function solution(input, output) {}
+function solution(input, output) {
 
-export default { id: "010_sumOfDigits", task, solution, testCases };
+    var sln = input.toString();
+    var sum = 0;
+
+    for (let i = 0; i < sln.length; i++) {
+        sum = sum + parseInt(sln[i]);
+    }
+    output.write(sum)
+
+}
+
+export default {
+    id: "010_sumOfDigits",
+    task,
+    solution,
+    testCases
+};
