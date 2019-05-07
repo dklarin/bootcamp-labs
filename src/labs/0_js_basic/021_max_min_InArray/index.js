dd0724c35e2228fc2ff1f04c7c3c5f6a63bf542e
dklarin <danijel.klarin@gmail.com>
2019-05-07 20:51:17 +0200

    Max and min from array

--- src/labs/0_js_basic/021_max_min_InArray/index.js
+++ src/labs/0_js_basic/021_max_min_InArray/index.js
@@ -5,6 +5,17 @@ const task = `
 /**the test case inputs */
 var testCases = [[12, 5, -1, 14, 32, 7]];
 
-function solution(input, output) {}
+function solution(input, output) {
+
+        input.sort(compareNumbers)
+        
+        output.write("This is min number: " + input[0])
+        output.write("This is max number: " + input[input.length - 1])
+
+}
+
+function compareNumbers(a, b) {
+        return a - b;
+}
 
 export default { id: "021_max_min_InArray", task, solution, testCases };
